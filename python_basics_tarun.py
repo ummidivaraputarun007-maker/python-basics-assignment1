eae7d08d37c2d0f3f6cd2cb132153731fcea7604
ummidivaraputarun007-maker <ummidivaraputarun007@gmail.com>
2026-04-15 19:15:17 +0530

    Above is the code to execute

---
+++ python_basics_tarun.py
@@ -0,0 +1,23 @@
+# Task 1: Variables and Data Types
+name = "Tarun"
+age = 18
+height = 1.75
+is_student = True
+
+print(name, age, height, is_student)
+
+
+# Task 2: String Formatting
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+
+# Task 3: Arithmetic Operations
+age_in_months = age * 12
+age_in_days = age * 365
+remainder = age % 7
+age_square = age ** 2
+
+print("Age in months:", age_in_months)
+print("Age in days:", age_in_days)
+print("Remainder when age divided by 7:", remainder)
+print("Age squared:", age_square)
